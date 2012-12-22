dcf12aee53291b3e67afc25ea59bfb245026cb1e
ra.pararede@gmail.com <ra.pararede@gmail.com@49a20295-3b20-14af-9272-398ff5c85ab8>
2012-12-22 00:31:10 +0000

    configured force layout

--- js/UserView.js
+++ js/UserView.js
@@ -1,22 +1,32 @@
 define(['jquery', 'underscore', 'backbone', 'd3', 'User', 'Channels'], function($, _, Backbone, d3, User, Channels) {
     "use strict";
     return Backbone.View.extend({
-        width: '100%',
+        width: 960,
         height: 500,
 
-        container: d3.select('section#content').append('svg'),
+        svg: d3.select('section#content').append('svg'),
+
+        force: null,
+        nodes: [],
+        links: [],
 
         initialize: function() {
-            this.collection.on('user.profile.loaded', this.render, this);
+            var self = this;
+            this.collection.on('user.profile.loaded', function(){
+                this.render();
+            }, this);
 
-            this.container.attr('width', this.width).attr('height', this.height);
+            this.svg.attr('width', this.width).attr('height', this.height);
 
+            this.force = d3.layout.force()
+                .size([this.width, this.height])
+                .charge(-500)
+                .linkDistance(150);
         },
 
         getNextUserId: function(){
             var
                 nextUserId = null,
-
                 users = this.collection,
 
                 loadedUsers = users.where({
@@ -39,7 +49,7 @@ define(['jquery', 'underscore', 'backbone', 'd3', 'User', 'Channels'], function(
                         }, this);
                         if(!nextUserId){
                             loadedUser.set('processed', true);
-                            loadedUsers.remove(loadedUser, {silent:true});
+                            users.remove(loadedUser, {silent:true});
                         }else{
                             return true;
                         }
@@ -50,43 +60,107 @@ define(['jquery', 'underscore', 'backbone', 'd3', 'User', 'Channels'], function(
         },
 
         render: function() {
-            console.log('rendering users : %o',this.collection.models);
-            var
-                linkedChannels = new Channels(),
-                nextUserId = null;
+            var self = this;
 
-            this.collection.each(function(user, index){
-                var userLinkedChannels = user.get('linkedChannels');
-                //merge channels from all users
-                linkedChannels.add(userLinkedChannels.toJSON(),{silent:true});
+            var users = this.collection;
+            this.nodes = [];
+            this.links = [];
+            users.each(function(user, uid){
+                user.x = user.x || Math.floor(Math.random() * self.width);
+                user.y = user.y || Math.floor(Math.random() * self.height);
+                if(user.has('linkedChannels')){
+                    user.get('linkedChannels').each(function(linkedChannel){
+                        var target = users.get(linkedChannel.id);
+                        if(target){
+                            self.links.push({
+                                source: user,
+                                target: target
+                            });
+                        }
+                    });
+                }
+                self.nodes.push(user);
             });
 
-            nextUserId = this.getNextUserId();
+            this.force
+                .nodes(this.nodes)
+                .links(this.links);
+
+            var node = this.svg.selectAll('circle.node')
+                .data(this.force.nodes(), function(user, index) {
+                    return user.id;
+                });
+
+            var nodeEnter = node.enter()
+                .append('circle')
+                .attr('class', 'node')
+                .attr('id', function(d, i){
+                    return d.id;
+                })
+                .attr('r', function(d, i) {
+                    return Math.max((d.get('linkedChannels') || []).length, 5);
+                })
+                .style("fill", function(d, i) {
+                    return 'rgb(' + parseInt(255 - 100 * Math.random(),10) + ',' + parseInt(255 - 100 * Math.random(),10) + ',' + parseInt(255 - 100 * Math.random(),10) + ')';
+                });
+
+            nodeEnter.append('title').text(function(d) { return d.id; });
+            nodeEnter.call(this.force.drag);
+
+            node.exit().remove();
+
+            var link = this.svg.selectAll('line.link')
+                .data(this.force.links(), function(link, index) {
+                    return link.source.id + '-' + link.target.id;
+                });
+
+            link.enter().append('line').attr('class', 'link');
+            link.exit().remove();
+
+            this.force.on('tick', function() {
+                node
+                    .attr("cx", function(d) {
+                        return d.x;
+                    })
+                    .attr("cy", function(d) {
+                        return d.y;
+                    });
+
+                link
+                    .attr("x1", function(d) {
+                        return d.source.x;
+                    })
+                    .attr("y1", function(d) {
+                        return d.source.y;
+                    })
+                    .attr("x2", function(d) {
+                        return d.target.x;
+                    })
+                    .attr("y2", function(d) {
+                        return d.target.y;
+                    });
+            });
+
+            this.force.start();
+
+            var nextUserId = this.getNextUserId();
+
             console.warn('next user id : %o', nextUserId);
-
-            var content = this.container.selectAll('circle').data(linkedChannels.models, function(linkedChannel, index) {
-                return linkedChannel.id;
-            });
-
-            content.enter().append('circle').attr('cx', function(d, i) {
-                return parseInt(300 * Math.random(), 10) + i * 30;
-            }).attr('cy', function(d, i) {
-                return 100 + i * 30;
-            }).attr('r', function(d, i) {
-                return parseInt(d.get('subscribers').replace(',', ''),10) / 1000;
-            }).style("fill", function(d, i) {
-                return 'rgb(' + parseInt(255 - 100 * Math.random(),10) + ',' + parseInt(255 - 100 * Math.random(),10) + ',' + parseInt(255 - 100 * Math.random(),10) + ')';
-            });
 
             if(nextUserId){
                 var nextUser = new User({
                     id : nextUserId
                 });
-                console.warn('adding user %o - %o', nextUser, this.collection.length);
-                this.collection.add(nextUser);
-                if(this.collection.length <= 7){
-                    console.log('calling next profile');
+
+                var users = this.collection;
+                users.add(nextUser);
+
+                if(users.length <= 15){
+                    console.log('calling next profile %o', nextUser );
                     nextUser.load();
+
+                }else{
+                    users.remove(nextUser);
                 }
             }
 
